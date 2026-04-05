d89117dd6cd0f8106c0468c22bb444847d99fa97
Guilherme Braga <gbbraga.dev@gmail.com>
2026-04-05 17:46:08 -0300

    fix: repair alembic revision graph

--- alembic/versions/e3e606f12b81_add_missing_indexes_for_jobs_and_credit_.py
+++ alembic/versions/e3e606f12b81_add_missing_indexes_for_jobs_and_credit_.py
@@ -1,17 +1,16 @@
 """add missing indexes for jobs and credit_purchases
 
 Revision ID: e3e606f12b81
-Revises: d697edf17fc9
+Revises: f2b6c6a9d51b
 Create Date: 2026-04-05 17:17:04.334758
 """
+
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
 
-
-revision: str = 'e3e606f12b81'
-down_revision: Union[str, None] = 'd697edf17fc9'
+revision: str = "e3e606f12b81"
+down_revision: Union[str, None] = "f2b6c6a9d51b"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
